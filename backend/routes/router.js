594d892e8f5988087640bb0b15a59e543a8259e0
dounya mellouki <93975469+dounya-m@users.noreply.github.com>
2023-03-06 11:00:31 +0100

    create backend user admin and company

---
+++ backend/routes/router.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const api = '/api/MFN/'
+
+router.use(`${api}admin`, require('./admin/AdmineRoute'))
+router.use(`${api}user`, require('./user/UserRoutes'))
+router.use(`${api}company`, require('./company/CompanyRoutes'))
+
+module.exports = router
